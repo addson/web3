10706b43996c49896f64d3a71c53651ec7069e6e
addson <addson@gmail.com>
2024-05-21 17:17:48 -0300

    Added the txInputs and txOutputs arrays in Transaction

--- blockchain/src/lib/transaction.ts
+++ blockchain/src/lib/transaction.ts
@@ -2,6 +2,7 @@ import TransactionType from './transactionType';
 import sha256 from 'crypto-js/sha256';
 import Validation from './validation';
 import TransactionInput from './transactionInput';
+import TransactionOutput from './transactionOutput';
 
 /**
  * Transaction class to represents a block transaction.
@@ -11,29 +12,45 @@ export default class Transaction {
   type: TransactionType;
   timestamp: number; //always UTC without fuso
   hash: string;
-  txInput: TransactionInput | undefined;
-  to: string;
+  txInputs: TransactionInput[] | undefined;
+  txOutputs: TransactionOutput[];
 
   constructor(tx?: Transaction) {
     this.type = tx?.type || TransactionType.REGULAR;
     this.timestamp = tx?.timestamp || Date.now();
-    this.to = tx?.to || '';
 
-    if (tx && tx.txInput) {
-      /* istanbul ignore next */ //this sentence before ignores the next line to coverage tests
-      this.txInput = new TransactionInput(tx?.txInput);
-    } else {
-      this.txInput = undefined;
-    }
+    //initializes the txInput array
+    this.txInputs = tx?.txInputs
+      ? tx.txInputs.map(txi => new TransactionInput(txi))
+      : undefined;
+
+    //initializes the txOutput array
+    this.txOutputs = tx?.txOutputs
+      ? tx.txOutputs.map(txo => new TransactionOutput(txo))
+      : [];
 
     this.hash = tx?.hash || this.getHash();
+
+    //ever txOutput has this transaction hash
+    this.txOutputs.forEach((txo, index, array) => {
+      array[index].transactionHash = this.hash;
+    });
   }
 
   /** It will generates a unique assign to this transaction. */
   getHash(): string {
-    const txInputHash = this.txInput ? this.txInput.getHash() : '';
+    const txInputHashs =
+      this.txInputs && this.txInputs.length
+        ? this.txInputs.map(txi => txi.signature).join(',')
+        : '';
+
+    const txOutputHashs =
+      this.txOutputs && this.txOutputs.length
+        ? this.txOutputs.map(txo => txo.getGash()).join(',')
+        : '';
+
     return sha256(
-      this.type + txInputHash + this.to + this.timestamp,
+      this.type + txInputHashs + txOutputHashs + this.timestamp,
     ).toString();
   }
 
@@ -44,17 +61,46 @@ export default class Transaction {
     if (this.hash !== this.getHash())
       return new Validation(false, 'Invalid Hash');
 
-    if (!this.to) return new Validation(false, 'Invalid EMPTY to');
+    if (
+      !this.txOutputs ||
+      !this.txOutputs.length ||
+      this.txOutputs.map(txo => txo.isValid()).some(v => !v.success)
+    )
+      return new Validation(false, 'Invalid (TXO) TransactionOutput');
 
-    if (this.txInput) {
-      const validation = this.txInput.isValid();
-      if (!validation.success) {
+    if (this.txInputs && this.txInputs.length) {
+      const validationsTxiFalse = this.txInputs
+        .map(txi => txi.isValid())
+        .filter(v => !v.success);
+      if (validationsTxiFalse && validationsTxiFalse.length) {
+        const message = validationsTxiFalse.map(v => v.message).join(' ');
+        return new Validation(false, `Invalid TxInput as: ${message}`);
+      }
+
+      const sumTxInputs = this.txInputs
+        .map(txi => txi.amount)
+        .reduce((a, b) => a + b, 0);
+
+      const sumTxOutputs = this.txOutputs
+        .map(txo => txo.amount)
+        .reduce((a, b) => a + b, 0);
+
+      if (sumTxInputs < sumTxOutputs) {
         return new Validation(
           false,
-          `Invalid transaction as: ${validation.message}`,
+          `Invalid amounts: TxInputs Amounts < TxOutputs Amounts  `,
         );
       }
     }
+
+    if (this.txOutputs.some(txo => txo.transactionHash !== this.hash)) {
+      return new Validation(
+        false,
+        `Invalid TxOutput reference hash. It should be equals to this transaction hash. `,
+      );
+    }
+
+    //todo validates the fees and rewards when tx type equals FEE
 
     return new Validation();
   }
---
+++ blockchain/src/lib/transactionOutput.ts
@@ -0,0 +1,31 @@
+import Validation from './validation';
+import sha256 from 'crypto-js/sha256';
+
+/**
+ * TransactionOutput class
+ */
+export default class TransactionOutput {
+  toAddress: string;
+  amount: number;
+  transactionHash?: string;
+
+  constructor(txOutput?: TransactionOutput) {
+    this.toAddress = txOutput?.toAddress || '';
+    this.amount = txOutput?.amount || 0;
+    this.transactionHash = txOutput?.transactionHash || '';
+  }
+
+  isValid(): Validation {
+    if (this.amount < 1) {
+      return new Validation(false, 'Negative amount is invalid!');
+    }
+
+    return new Validation();
+  }
+
+  getGash(): string {
+    return sha256(
+      this.toAddress + this.amount + this.transactionHash,
+    ).toString();
+  }
+}
